d0040722aa208a3e94815cbbf8d39ec0a20ad32f
kevokevoh <kevnmacharia@gmail.com>
2018-10-17 10:27:06 +0300

    Minor highlight

--- cardIndex.js
+++ cardIndex.js
@@ -11,12 +11,11 @@ var token = visa({
 });
 
 var options = {
-    "keyId": "08GdpX3Vu3hX9CB8k2RB6FP548oc7DBH",
+    "keyId": "08jbVj4tbz3N9VVK30Qu7AshZyuDe3qe",
     "cardDetails": {
         "cardNumber": "ejbhIpMEgYnIODcB4//rrVxMHrqHcnLD6pDRF36jlEk72bETAfiOoxmpI9pGiidqMmkgAnvJfVgR3CLAV5EdG4Mu5IWK26QRnVtwvsVEUtpah7IylbxV9MLvXh2FjIJskKCWNLidb1G4PN5963hnV3IoZd2pF99JwV9lPhOHT5ymlNeg7sTzQQDN1I0/yJApds+t79hl9QVp4PusUDfSsPQTtR2frzlH4V3W+XjHDhmy5oNhiUaVxv27cyG1SWeCKkVC9tc8zLy4pvlgoplrLV8JRaS9hfWalJjv2xtk3DXmNT2urtFv2evcI3LM/S29KlJjPXZcBp0IYyB/taunCA==",
         "cardType": "001",
-        "cardExpirationYear": "2031",
-        
+        "cardExpirationYear": "2031"
     }
 };
 token.tokenizeCard(options, function(err, resp, data) {
